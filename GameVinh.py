91c15b0e327c89607be64be88376687b55fc38f6
vinhdq2407 <vinhdq2407@gmail.com>
2022-04-21 13:34:06 +0700

    no

--- GameVinh.py
+++ GameVinh.py
@@ -4,7 +4,6 @@ import pygame,sys,random
 def draw_floor():
     screen.blit(floor,(floor_x_pos,650))
     screen.blit(floor,(floor_x_pos+432,650))
-    # vinh sua
 
 # hàm tạo ống liên tục
 def create_pipe():
@@ -21,11 +20,20 @@ def move_pipe(pipes):
 # hàm vẽ ống
 def draw_pipe(pipes):
     for pipe in pipes:
-        if pipe.bottom >= 768 : 
+        if pipe.bottom >= 600 : 
             screen.blit(pipe_surface,pipe)
         else:
             flip_pipe = pygame.transform.flip(pipe_surface,False,True)
             screen.blit(flip_pipe,pipe)
+#hàm xử lý va chạm
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            print(pipe)
+            return False
+    if bird_rect.top <= -75 or bird_rect.bottom >= 650:
+        return False  
+    return True
 pygame.init()
 # set khung hình 
 screen = pygame.display.set_mode((432,768))
@@ -34,6 +42,8 @@ clock = pygame.time.Clock()
 gravity = 0.25
 bird_movement = 0
 
+# game ddang hoat dong
+game_active = True
 # chèn background
 bg = pygame.image.load('C:/Users/Admin/OneDrive/Máy tính/FlappyBird_Project/imgV2/background-night.png').convert()
 bg = pygame.transform.scale2x(bg)
@@ -54,7 +64,7 @@ pipe_list = []
 # tao timer (tao ong xuat hien ngau nhien)
 spawnpipe = pygame.USEREVENT
 pygame.time.set_timer(spawnpipe,1200)
-pipe_height = [200,300,400]
+pipe_height = [100,200,300]
 
 # tạo chuyển động background
 floor_x_pos  = 0
@@ -66,19 +76,30 @@ while True:
             pygame.quit()
             sys.exit()
         if(event.type == pygame.KEYDOWN):
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement = -11
+            if event.key == pygame.K_F1 and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100,384)
+                bird_movement = 0
         if event.type == spawnpipe:
             pipe_list.extend(create_pipe())
+
     screen.blit(bg,(0,0))
-    # chim
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird,bird_rect)
-    # ống
-    pipe_list = move_pipe(pipe_list)
-    draw_pipe(pipe_list)
+    
+    if game_active:
+        # chim
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird,bird_rect)
+        game_active= check_collision(pipe_list)
+        # ống
+        pipe_list = move_pipe(pipe_list)
+        draw_pipe(pipe_list)
+
+    
     # sàn
     floor_x_pos -= 1
     draw_floor()
